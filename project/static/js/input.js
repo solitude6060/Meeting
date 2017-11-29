01258549c45873183db8435a72f6264e8ae8fd5d
Jiabin95 <ongjiabin@gmail.com>
2017-11-29 16:12:03 +0800

    change meeting_room

---
+++ project/static/js/input.js
@@ -0,0 +1,21 @@
+$( ".field" ).hover(
+  function() {
+      $(this).addClass("field-expanded");
+  },
+  function() {
+    	if($( this ).find( ".text-area" ).val() == ""  && !$(this).find(".text-area").is(':focus')){
+    	$(this).removeClass("field-expanded");
+      }
+  }
+)
+
+$( ".field" ).click(function() {
+  $(this).addClass("field-expanded");
+});
+
+$( ".text-area" ).blur(
+  function() {
+      if($( this ).val() == "")
+        $( this ).parent().removeClass("field-expanded");
+  }
+);
