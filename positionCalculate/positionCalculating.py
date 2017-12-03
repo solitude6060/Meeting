0d31fb3f6379944db0824ad7cab324094b3ee063
Margo <solitude6060@gmail.com>
2017-12-03 22:35:43 +0800

    low the weight in positioning

--- positionCalculate/positionCalculating.py
+++ positionCalculate/positionCalculating.py
@@ -84,7 +84,7 @@ for meeting in meeting_all_today:
                     if seat_wifi is not None:
                         for i in seat_wifi:
                             s_wifi = i[5]
-                            weight = s_wifi*-1
+                            weight = s_wifi*-1*0.1
                             dis = weight*((s_wifi - wifi)**2)
                             Matrix[x][y] = dis
 
