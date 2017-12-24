c70fa53628dc1e408e85175c0d64e24e0ae33352
Margo <solitude6060@gmail.com>
2017-12-24 20:36:05 +0800

    seat table change dynamic by detecting the max seats

--- project/templatetags/list_filter.py
+++ project/templatetags/list_filter.py
@@ -8,7 +8,7 @@ def get_at_index(list, x, y, n, m):
     #list=> Name = [[0 for y in range(1,ymax+1)] for x in range(1,xmax+1)]
     #x = 1.....xmax+1, y = ymax+1......1
     #n = length(x), m = length(y)
-    m = m - n
-    y = abs(m-y)
+    m = abs(m - n)
+    y = abs(m - y)
     x = x - 1
     return list[x][y]
